9939a985b2907a7ae6bc60ceb0c5d95198156e37
Sayjal <ssayjal11@gmail.com>
2023-08-10 22:28:23 +0545

    Home Page

---
+++ home.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Tech</title>
+    <link rel="stylesheet" href="css/home.css" >
+</head>
+<body>
+    <header>
+        <div class="main">
+            <div class="logo">
+                <a href="home.php"><img src="logo.jpg" href="home.php"></a>
+            </div>
+            <ul>
+                <li class="active"><a href="home.php">Home</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="signup.php">Study Material</a></li>
+                <li><a href="login.php">Log In</a></li>
+            </ul>
+        </div>
+        <div class="title">
+            <h1>EASY LEARNING</h1>
+            <h6>Learn from home, prepare for your exams and improve your grades. You can get access to importants study materials out here.</h6>
+        </div>
+        <div class="button">
+            <a href="signup.php" class="btn">Get Started</a>
+        </div>
+    </header>
+</body>
+</html>
