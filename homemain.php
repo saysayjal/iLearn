c42abae75ccf755d12511ef6f8960143e0d9ea2e
Sayjal <ssayjal11@gmail.com>
2023-08-11 09:46:48 +0545

    Home page 2

---
+++ homemain.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Tech</title>
+    <link rel="stylesheet" href="css/home.css" >
+</head>
+<body>
+    <header>
+        <div class="main">
+            <div class="logo">
+                <a href="homemain.php"><img src="logo.jpg"></a>
+            </div>
+            <ul>
+                <li class="active"><a href="homemain.php">Home</a></li>
+                <li><a href="aboutusmain.php">About Us</a></li>
+                <li><a href="studymaterialmain.php">Study Material</a></li>
+                <li><a href="home.php">Log Out</a></li>
+            </ul>
+        </div>
+        
+        <div class="title">
+            <h1>EASY LEARNING</h1><br>
+            <h6>Welcome to ILEARN.
+                We provide important study materials to improve grades of students.
+            </h6>
+            <div class="buttonmain">
+            <a href="aboutusmain.php" class="btnmain">See More</a>
+        </div>
+        </div>
+        
+        
+    </header>
+    
+</body>
+</html>
