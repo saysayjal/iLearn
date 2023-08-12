7c5ce299f1ba66a12874d66251088ad4f644d64e
Sayjal <ssayjal11@gmail.com>
2023-08-12 09:31:08 +0545

    Study Material Page

--- studymaterial.php
+++ studymaterial.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
+<head> 
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -24,3 +24,4 @@
     </header>
 </body>
 </html>
+</html> 
