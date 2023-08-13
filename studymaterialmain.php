e25e6859d4c02696fec77e2ce2611fceed912033
Sayjal <ssayjal11@gmail.com>
2023-08-13 08:35:56 +0545

    another study material page

---
+++ studymaterialmain.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Tech</title>
+    <link rel="stylesheet" href="css/studymaterial.css" >
+</head>
+<body>
+    <header>
+        <div class="main">
+            <div class="logo">
+                <a href="homemain.php"><img src="logo.jpg" href="home.php"></a>
+            </div>
+            <ul>
+                <li><a href="homemain.php">Home</a></li>
+                <li><a href="aboutusmain.php">About Us</a></li>
+                <li class="active"><a href="studymaterialmain.php">Study Material</a></li>
+                <li><a href="home.php">Log Out</a></li>
+            </ul>
+        </div>          
+        <br><br>
+    <section class="course">
+        <h4>Courses we offer</h4>
+        <p>This website provides complete set of reference materials such as notes, syllabus and more for B.Sc. CSIT students.</p>
+        
+        <div class="row">
+            <div class="course-col">
+                <h3>First Semester</h3>
+                <p>First semester in B.Sc. CSIT helps to build the basic foundation of computer science, electronic circuits, programming and mathematics to get you started with the journey of B.Sc. CSIT</p>
+            <a href="first.php">View pdf</a> 
+
+            </div>
+            <div class="course-col">
+                <h3>Second Semester</h3>
+                <p>The second semester of B.Sc. CSIT focuses on fundamental concepts of problem solving, object oriented programming approach and advanced concepts of electronic circuits and boards.</p>
+                <a href="first.php">View pdf</a> 
+            </div>
+            <div class="course-col">
+                <h3>Third Semester</h3>
+                <p>The third semester of B.Sc. CSIT focuses on foundation of data structures, mathematical modeling, architecture of computers, concepts of graphics and statistical measures including hypothesis and various tests.</p>
+                <a href="first.php">View pdf</a> 
+            </div>
+            <div class="course-col">
+                <h3>Fourth Semester</h3>
+                <p>Fourth semester of B.Sc. CSIT introduces state machines and their languages, concept of the computer networks and operating systems, foundation of database management system and glimpse of underlying principles of artificial intelligence</p>
+                <a href="first.php">View pdf</a> 
+            </div>
+        </div>
+</section>
+</header>
+</body>
+</html>
