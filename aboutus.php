a6cd23b03a232137a4d0301c7177994098d739e7
Sayjal <ssayjal11@gmail.com>
2023-08-14 20:35:37 +0545

    About Us Page

---
+++ aboutus.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Tech</title>
+    <link rel="stylesheet" href="css/aboutus.css" >
+</head>
+<body>
+    <header>
+        <div class="main">
+            <div class="logo">
+                <a href="home.php"><img src="logo.jpg" href="home.php"></a>
+            </div>
+            <ul>
+                <li><a href="home.php">Home</a></li>
+                <li class="active"><a href="aboutusmain.php">About Us</a></li>
+                <li><a href="signup.php">Study Material</a></li>
+                <li><a href="login.php">Log In</a></li>
+            </ul>
+        </div>
+
+        <div class="title">
+            <br><br><br><br><br><br><h1>WHAT WE DO</h1><br>
+            <h3>Welcome to ILEARN.We provide important study materials to improve grades of students.</h3>
+            <br><br><br>
+            <h5>Objective:</h5><br>
+            <p><h3>The main objective of this website is to provide students intensive knowledge and skill on different
+            areas of computer science and information technology including design, theory, programming and application of
+            computer system. It is envisaged that graduate of this program will be equipped with necessary knowledge of 
+            computer software and hardware system</h3></p>
+            
+        </div>
+        </div>
+        
+    </header>
+</body>
+</html>
