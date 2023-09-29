b403fa2e73d7440a7f01e052a7705500a5ed5b0b
Sayjal <ssayjal11@gmail.com>
2023-09-29 08:51:26 +0545

    added files

---
+++ manage-admin.php
@@ -0,0 +1,36 @@
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="css/admin.css">
+    </head>
+    <body>
+        <!--Menu section starts -->
+        <div class="menu text-center">
+            <div class="wrapper">
+                <ul>
+                    <li><a href="manage-service.php">Services</a></li>
+                    <li><a href="manage-admin.php">Admin</a></li>
+                    <li><a href="admin.php">Logout</a></li>
+                </ul>
+            </div>
+
+        </div>
+        <div>
+            <br><br>
+        <a href="add-admin.php" class="btn-primary">Add admin</a>
+
+            <br>
+            <br>
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+
+            </div>
+</body>
+</html>
+
+    
