03c6037bd5c7a7942a37db33cca5b10fb9d2552c
Sayjal <ssayjal11@gmail.com>
2023-10-20 11:48:02 +0545

    added files

---
+++ first.php
@@ -0,0 +1,62 @@
+<?php include('config.php');?>
+<html>
+    <header>
+        <title></title>
+        <link rel="stylesheet" href="css/studymaterial.css">
+
+    </header>
+    <body>
+    <header>
+        <div class="main">
+           
+            <ul>
+                <li><a href="homemain.php">Home</a></li>
+                <li><a href="aboutusmain.php">About Us</a></li>
+                <li class="active"><a href="studymaterialmain.php">Study Material</a></li>
+                <li><a href="home.php">Log Out</a></li>
+            </ul>
+        </div>          
+        <br><br>
+        <section class="course"> <br>
+            <h2>Download pdf</h2>
+            <p><b>Click here to download pdf</b></p><br><br>
+            <div class="row">
+            <div class="course-col1">
+            <h4>Digital Logics: </h4><br>
+            <a href="Unit1.pdf" target="_blank">Unit 1</a><br>
+            <a href="Unit2.pdf" target="_blank">Unit 2</a><br>
+            <a href="Unit3.pdf" target="_blank">Unit 3</a><br>
+            </div>
+            <div class="course-col1">
+            <h4>C programming: </h4><br>
+            <a href="bim.pdf" target="_blank">Unit 1</a><br>
+            <a href="bim.pdf" target="_blank">Unit 2</a><br>
+            <a href="bim.pdf" target="_blank">Unit 3</a><br>
+            </div>
+            <div class="course-col1">
+            <h4>IIT: </h4><br>
+            <a href="bim.pdf" target="_blank">Unit 1</a><br>
+            <a href="bim.pdf" target="_blank">Unit 2</a><br>
+            <a href="bim.pdf" target="_blank">Unit 3</a><br>
+            </div>
+            <div class="course-col1">
+            <h4>Mathematics: </h4><br>
+            <a href="bim.pdf" target="_blank">Unit 1</a><br>
+            <a href="bim.pdf" target="_blank">Unit 2</a><br>
+            <a href="bim.pdf" target="_blank">Unit 3</a><br>
+            </div>
+            <div class="course-col1">
+            <h4>Physics: </h4><br>
+            <a href="bim.pdf" target="_blank">Unit 1</a><br>
+            <a href="bim.pdf" target="_blank">Unit 2</a><br>
+            <a href="bim.pdf" target="_blank">Unit 3</a><br>
+            </div>
+</div>
+</section>   
+</header>
+</body>
+</html>
+
+
+
+    
